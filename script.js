3ff21229e62321d5e246f77dd27880a309b38787
rajesh01ntpc <148991688+rajesh01ntpc@users.noreply.github.com>
2024-02-23 19:24:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Validate contact form
+const form = document.querySelector('#contact-form');
+form.addEventListener('submit', (e) => {
+    // Your validation logic here
+    e.preventDefault();
+});
